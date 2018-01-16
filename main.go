cd4c3ac63c5c15237ddd6ecdcc125db3b153d5b4
Matthias Hannig <matthias@hannig.cc>
2018-01-16 22:47:48 +0100

    added lights and state

--- main.go
+++ main.go
@@ -38,12 +38,56 @@ func parseFlags() *Config {
 	return config
 }
 
+// Handle light state
+type Light struct {
+	Value int
+}
+
+type LightsState map[string]Light
+
+/*
+ Initialize lights state:
+ Assign handles to light value
+*/
+func NewLightsState() *LightsState {
+	state := &LightsState{
+		"entry":     Light{0},
+		"foh":       Light{0},
+		"desk_wall": Light{0},
+		"desk_bar":  Light{0},
+	}
+
+	return state
+}
+
+func (self *LightsState) Set(handle string, value int) {
+	(*self)[handle] = Light{value}
+
+	// TODO: Talk to dali
+}
+
+func (self *LightsState) Read(handle string) int {
+	// TODO: Talk to dali
+
+	return (*self)[handle].Value
+}
+
+func (self *LightsState) Refresh() {
+	for handle, _ := range *self {
+		fmt.Println("Refreshing state of", handle)
+		self.Read(handle)
+	}
+}
+
 func main() {
 
 	fmt.Println("Starting dali to mqtt bridge")
 
 	// Initialize configuration
 	config := parseFlags()
+
+	lights := NewLightsState()
+	lights.Refresh()
 
 	fmt.Println(config)
 
