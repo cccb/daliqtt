bd9d1bc2e33d5f51de202305675b7e6cd44c7dfb
Matthias Hannig <matthias@hannig.cc>
2018-01-17 12:18:35 +0100

    added simple server stub

---
+++ stub/server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+// Global state
+var lights []int = []int{23, 42, 0, 254}
+
+func parseCommand(query string) (int, int, error) {
+	cmd, err := url.QueryUnescape(query)
+	if err != nil {
+		log.Println("An error occured:", err)
+		return 0, 0, err
+	}
+
+	tokens := strings.Split(cmd, " ")
+	if len(tokens) != 4 {
+		return 0, 0, fmt.Errorf("Invalid command")
+	}
+
+	id, err := strconv.ParseInt(tokens[1], 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	value, err := strconv.ParseInt(tokens[3], 10, 64)
+	if err != nil {
+		return int(id), 0, err
+	}
+
+	return int(id), int(value), nil
+}
+
+func handleRetrieve(req *http.Request) []byte {
+	res := ""
+	for _, v := range lights {
+		res += fmt.Sprintf("%d ", v)
+	}
+
+	res += "\r\n"
+
+	return []byte(strings.TrimSpace(res))
+}
+
+func handleUpdate(req *http.Request) []byte {
+	id, value, err := parseCommand(req.URL.RawQuery)
+	if err != nil {
+		return []byte(err.Error())
+	}
+
+	if id >= len(lights) {
+		return []byte("ERROR")
+	}
+
+	log.Println("Updating", id, "set to", value)
+	lights[id] = value
+
+	return []byte("")
+}
+
+func main() {
+	fmt.Println("Starting licht.cgi stub server")
+
+	_ = lights
+
+	http.HandleFunc(
+		"/cgi-bin/licht.cgi",
+		func(res http.ResponseWriter,
+			req *http.Request) {
+			if req.Method == "POST" {
+				res.Write(handleUpdate(req))
+			} else {
+				res.Write(handleRetrieve(req))
+			}
+		})
+
+	http.ListenAndServe("localhost:2299", nil)
+}
