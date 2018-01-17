020ca9aeb22553699483b5516e5fa9d8bd5c531e
Matthias Hannig <matthias@hannig.cc>
2018-01-17 22:25:03 +0100

    removed old code

--- light.go
+++ light.go
@@ -1,49 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import ()
 
 // Handle light state
 type Light struct {
 	Id    int
 	Value int
-}
-
-type LightsState map[string]Light
-
-/*
- Initialize lights state:
- Assign handles to light value
-*/
-func NewLightsState() *LightsState {
-	state := &LightsState{
-		"entry":     Light{0, 0},
-		"foh":       Light{1, 0},
-		"desk_wall": Light{2, 0},
-		"desk_bar":  Light{3, 0},
-	}
-
-	return state
-}
-
-func (self *LightsState) Set(handle string, value int) {
-	light := (*self)[handle]
-	light.Value = value
-	(*self)[handle] = light
-
-	// TODO: Talk to dali
-}
-
-func (self *LightsState) Read(handle string) int {
-	// TODO: Talk to dali
-
-	return (*self)[handle].Value
-}
-
-func (self *LightsState) Refresh() {
-	for handle, _ := range *self {
-		fmt.Println("Refreshing state of", handle)
-		self.Read(handle)
-	}
 }
