d9ebfbbeac20ff431929e3005e6d968386e21853
Matthias Hannig <matthias@hannig.cc>
2018-01-17 21:33:53 +0100

    load lights from server

--- lights_svc.go
+++ lights_svc.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"log"
-
-	"github.com/eclipse/paho.mqtt.golang"
 )
 
 type LightsSvc struct {
 	Lights []Light
+	Cgi    *LichtCgi
+}
 
-	mqttClient mqtt.Client
-	actions    chan Action
+func NewLightsSvc(lichtCgiBase string) *LightsSvc {
+	log.Println("Using licht.cgi @", lichtCgiBase)
+	// Load initial state from server
+	cgi := NewLichtCgi(lichtCgiBase)
+
+	lights, err := cgi.FetchLights()
+	if err != nil {
+		log.Println("Could not fetch lights from server:", err)
+	}
+
+	// Initial State
+	svc := &LightsSvc{
+		Lights: lights,
+		Cgi:    cgi,
+	}
+
+	return svc
+}
+
+/*
+ Service main: React to incoming actions and dispatch
+ responses.
+*/
+func (self *LightsSvc) Handle(actions chan Action, dispatch Dispatch) {
+	// Hanlde actions
+	for action := range actions {
+		switch action.Type {
+		case SET_LIGHT_VALUE_REQUEST:
+			dispatch(SetLightValueSuccess(23, 42))
+		case GET_LIGHT_VALUES_REQUEST:
+			dispatch(GetLightValuesSuccess(self.Lights))
+		}
+	}
 }
