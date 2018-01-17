96949f3efd71d2e498b264d042ee8394540da97f
Matthias Hannig <matthias@hannig.cc>
2018-01-17 12:38:22 +0100

    refined tests

--- lichtcgi_test.go
+++ lichtcgi_test.go
@@ -6,20 +6,39 @@ import (
 
 func TestFetchLights(t *testing.T) {
 	t.Log("Fetching lights")
-
-	cgi := NewLichtCgi("http://dali")
+	cgi := NewLichtCgi("http://localhost:2299")
 	lights, err := cgi.FetchLights()
 	if err != nil {
+		t.Log("licht.cgi stub server running?")
 		t.Error(err)
 	}
 
-	t.Log("Res:", lights)
+	if len(lights) != 4 {
+		t.Error("Expected 4 values")
+	}
+
+	t.Log("Success. Retrieved:", lights)
 }
 
 func TestSetLight(t *testing.T) {
-	cgi := NewLichtCgi("http://dali")
+	cgi := NewLichtCgi("http://localhost:2299")
 	err := cgi.Update(Light{2, 127})
 	if err != nil {
+		t.Log("licht.cgi stub server running?")
 		t.Error(err)
 	}
+
+	// Read, check update
+
+	lights, err := cgi.FetchLights()
+	if err != nil {
+		t.Log("licht.cgi stub server running?")
+		t.Error(err)
+	}
+
+	if lights[2].Value != 127 {
+		t.Error("Expected light 2 to be set to 127")
+	}
+
+	t.Log("Success. Retrieved:", lights)
 }
